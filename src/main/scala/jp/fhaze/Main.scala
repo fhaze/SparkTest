28f2350689a9182f5425386442ca3efa43d566fb
FHaze <eder_arm@icloud.com>
2019-08-09 22:58:38 -0300

    dynamic using validator factory by column name.

--- src/main/scala/jp/fhaze/Main.scala
+++ src/main/scala/jp/fhaze/Main.scala
@@ -2,38 +2,49 @@ package jp.fhaze
 
 import java.util.UUID
 
-import jp.fhaze.validator.{DummyValidator, ValidatorFactory}
+import jp.fhaze.validator.ValidatorFactory
 import org.apache.hadoop.fs.{FileSystem, FileUtil, Path}
 import org.apache.spark.sql.{DataFrame, Row, SparkSession}
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.sql.functions._
 
 object Main extends App {
+  // Create a uuid for creating temporary files
   val uuid = UUID.randomUUID().toString
 
+  // Define directories
   val inputFile  = s"hdfs/data/example.txt"
   val tempFile   = s"hdfs/data/out/example_${uuid}.txt"
   val outputFile = s"hdfs/data/out/output.txt"
   val errorFile  = s"hdfs/data/out/error.txt"
 
+  // Starts Spark Context
   val sparkConf = new SparkConf().setMaster("local[4]").setAppName("SparkTest")
   val ss = SparkSession.builder().config(sparkConf).getOrCreate()
   val sc = SparkContext.getOrCreate(sparkConf)
   val fs = FileSystem.get(sc.hadoopConfiguration)
 
-  import ss.implicits._
-
+  // Load example TXT into a DataFrame
   val example = ss.read
     .option("header", "true")
     .option("delimiter", "|")
     .csv(inputFile)
 
+  // Validate example TXT and create "_validated" columns with a bool containing "true" or "false"
   val validatedValues = Helper.validate(example)
-  val onlyGoodValues  = validatedValues.filter($"sex_validated" === true and $"id_validated" === true)
-  val onlyBadValues   = validatedValues.filter($"sex_validated" === false and $"id_validated" === false)
 
-  Helper.saveDataFrameToFileSystem(onlyGoodValues.drop("sex_validated", "id_validated"), outputFile)
-  Helper.saveDataFrameToFileSystem(onlyBadValues.drop("sex_validated", "id_validated"), errorFile)
+  // Put all "_validated" columns in an array
+  val validationFields = Helper.getValidatonFields(validatedValues)
 
+  // Filter "Good" and "Bad" values using "_validated" columns
+  val onlyGoodValues = validatedValues.filter(validationFields.map(col(_) === true).reduce(_ and _))
+  val onlyBadValues  = validatedValues.filter(validationFields.map(col(_) === false).reduce(_ or _))
+
+  // Save Dataset into fs
+  Helper.saveDataFrameToFileSystem(onlyGoodValues.drop(validationFields: _*), outputFile)
+  Helper.saveDataFrameToFileSystem(onlyBadValues.drop(validationFields: _*), errorFile)
+
+  // Preview results
   validatedValues.show()
 
   object Helper extends Serializable {
@@ -47,7 +58,13 @@ object Main extends App {
       val src = new Path(tempFile)
       val dst = new Path(destination)
 
+      if (fs.exists(dst))
+        fs.delete(dst, true)
       FileUtil.copyMerge(fs, src, fs, dst, true, sc.hadoopConfiguration, null)
+    }
+
+    def getValidatonFields(df: DataFrame) = {
+      df.columns.filter(_.endsWith("_validated"))
     }
 
     def validate(df: DataFrame) = {
